9ebeb83603fc7909a33e03101f2b9537f972cb09
YuLim2 <jyulim0120@gmail.com>
2022-05-10 20:38:19 +0900

    add: lib-sympy_example; permutations, combinations명

--- itertools_ex.py
+++ itertools_ex.py
@@ -1,5 +1,5 @@
-from itertools import permutations as P
-lists = [1, 2, 3]
+from itertools import permutations as P, combinations as C
+lists = list(range(1, 41))
 
-print(list(P(lists)))
-print(len(list(P(lists))))
+print(len(list(P(lists, 2))))
+print(len(list(C(lists, 3))))
